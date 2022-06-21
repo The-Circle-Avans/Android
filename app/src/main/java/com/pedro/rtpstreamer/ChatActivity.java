554fae9aa4ccf1f53842452bf5311e0e1f3461a0
TheElementalDragon <wesselwammes@hotmail.com>
2022-06-21 10:27:07 +0200

    gimme username

---
+++ app/src/main/java/com/pedro/rtpstreamer/ChatActivity.java
@@ -0,0 +1,15 @@
+package com.pedro.rtpstreamer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+public class ChatActivity extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
